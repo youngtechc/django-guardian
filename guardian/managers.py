ffcf6711ec8f03c122f70494e8f8e0d9a014e955
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-02-11 01:54:48 +0100

    Added TODO

--- guardian/managers.py
+++ guardian/managers.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from guardian.exceptions import ObjectNotPersisted
 
+# TODO: consolidate UserObjectPermissionManager and GroupObjectPermissionManager
 
 class BaseObjectPermissionManager(models.Manager):
 
